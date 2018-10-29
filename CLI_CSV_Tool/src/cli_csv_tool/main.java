94d43db63879a2e03df09fca613f7f5cf03a4c4d
xikaioliver <616651599@qq.com>
2018-10-29 19:46:33 +1100

    A workable version

--- CLI_CSV_Tool/src/cli_csv_tool/main.java
+++ CLI_CSV_Tool/src/cli_csv_tool/main.java
@@ -1,10 +1,26 @@
 package cli_csv_tool;
 
-public class main {
+import java.io.FileNotFoundException;
+import java.util.Arrays;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import pandas.*;
+
+public class Main {
+
+	public static void main(String[] args) throws FileNotFoundException {
 		
+		DataFrame df = new DataFrame();
+		
+		//Args: filename, sort_column1, sort_column2, ...
+		String filename = args[0];
+		String[] sortColumns = Arrays.copyOfRange(args, 1, args.length );
+		
+		//By default, the separator of the CSV file is ','. Otherwise extend this code.
+		if (args[0].endsWith(".csv")){
+			df = CSV_Reader.read_csv(args[0], ",");
+			df.sort(sortColumns);
+			df.print(3);
+		}
 	}
 
 }
